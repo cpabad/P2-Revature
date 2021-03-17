dcc22635d07f8d4e372d639610173673cfd970e6
ca132731@gmail.com <ca132731@gmail.com>
2021-03-17 13:39:19 -0500

    Pushing app-routing

--- iLearnAngular/src/app/app-routing.module.ts
+++ iLearnAngular/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< HEAD
+import { CreateLessonComponent } from './components/create-lesson/create-lesson.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: "create-lesson",
+    component: CreateLessonComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+=======
 import { CourseComponent } from './components/course/course.component';
 
 const routes: Routes = [
   {
     path:"course",
     component: CourseComponent
+>>>>>>> 8d08b9e922a1a55e346362816d7b13c7a2cd6f8f
   }
 ];
 
